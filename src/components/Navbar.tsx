22851e488b3a4a6e85849d860ac8e24db420cac6
Mmesomadavid <chukwunoyelummesoma1@gmail.com>
2025-05-20 22:47:03 +0100

    made new changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,39 +23,9 @@ const Navbar: React.FC = () => {
         <div className="flex justify-between items-center">
           {/* Logo */}
           <div className="flex items-center">
-            <div className="h-8 w-8 bg-blue-600 rounded flex items-center justify-center mr-2">
-              <svg 
-                width="16" 
-                height="16" 
-                viewBox="0 0 24 24" 
-                fill="none" 
-                xmlns="http://www.w3.org/2000/svg"
-                className="text-white"
-              >
-                <path 
-                  d="M12 4L4 8L12 12L20 8L12 4Z" 
-                  stroke="currentColor" 
-                  strokeWidth="2" 
-                  strokeLinecap="round" 
-                  strokeLinejoin="round"
-                />
-                <path 
-                  d="M4 16L12 20L20 16" 
-                  stroke="currentColor" 
-                  strokeWidth="2" 
-                  strokeLinecap="round" 
-                  strokeLinejoin="round"
-                />
-                <path 
-                  d="M4 12L12 16L20 12" 
-                  stroke="currentColor" 
-                  strokeWidth="2" 
-                  strokeLinecap="round" 
-                  strokeLinejoin="round"
-                />
-              </svg>
+            <div className="flex items-center justify-center mr-2">
+              <img src="https://bluescopeptyltd.club/assets/images/logoIcon/logo.png" alt="bluescope logo" draggable="false" className="h-auto w-44" />
             </div>
-            <span className="text-gray-900 font-bold text-xl">Hubpay</span>
           </div>
 
           {/* Desktop Navigation */}
