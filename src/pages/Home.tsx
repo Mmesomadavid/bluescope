78c1077de8f2398037ef101c086b6003448421ce
Mmesomadavid <chukwunoyelummesoma1@gmail.com>
2025-05-21 11:24:04 +0100

    made new changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,14 @@
 import type React from "react"
 import Hero from "../components/Hero"
-import TrustedByInvestors from "../components/TrustedByInvestors"
 import WhoWeAre from "../components/WhoWeAre"
+import Testimonials from "../components/Testimonials"
 
 const Home: React.FC = () => {
   return (
     <div className="min-h-screen">
       <Hero />
       <WhoWeAre />
-      <TrustedByInvestors />
+      <Testimonials />
     </div>
   )
 }
