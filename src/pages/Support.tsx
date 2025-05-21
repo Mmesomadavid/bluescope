11b3ccb9d254049685e7c7b88fce4dc517ffca70
Mmesomadavid <chukwunoyelummesoma1@gmail.com>
2025-05-22 00:11:41 +0100

    made new changes

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -1,8 +1,11 @@
+import Hero from "../components/Contact/Hero";
+import Contact from "../components/Contact/Contact";
 
 const Support = () => {
   return (
     <div>
-      
+      <Hero/>
+      <Contact/>
     </div>
   );
 }
