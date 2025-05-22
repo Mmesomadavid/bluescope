79493e653c65b137d9ceeb2715f06e90c53acc07
Mmesomadavid <chukwunoyelummesoma1@gmail.com>
2025-05-22 09:28:24 +0100

    made new changes

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,9 +1,11 @@
+import EventsSection from "../components/Events/Events";
 import Hero from "../components/Events/Hero";
 
 const Events = () => {
   return (
     <div>
       <Hero/>
+      <EventsSection/>
     </div>
   );
 }
